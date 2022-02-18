1ce342b26fb1c70058df7806b8fd07234f0ec621
Efe Erhan <efee@sfu.ca>
2022-02-18 15:05:25 -0800

    added files back to master

---
+++ app/src/main/java/com/group7/momio/mood_diary_result.kt
@@ -0,0 +1,11 @@
+package com.group7.momio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mood_diary_result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mood_diary_result)
+    }
+}
